9e2a8ca5d1b668acc736721182147c79b2893342
Italo Stuardo Cortez Silva <ingeniero.stuardo@gmail.com>
2021-04-27 05:50:56 -0600

    building TourList component and sass

--- src/components/TourList/TourList.jsx
+++ src/components/TourList/TourList.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './tourList.scss';
 
 import Tour from '../Tour/Tour';
 
+import { tourData } from '../../tourData';
+
 const TourList = () => {
+  const [tours, setTours] = useState(tourData);
+
+  const removeTour = id => {
+    console.log(id);
+  };
+
   return (
-    <div>
-      hello from TourList
-      <Tour />
-    </div>
+    <section className='tour-list'>
+      {tours.map(tour => (
+        <Tour key={tour.id} removeTour={removeTour} {...tour} />
+      ))}
+    </section>
   );
 };
 
