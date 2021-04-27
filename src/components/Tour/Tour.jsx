ac30e235aeefca1813a60d1d12b6b05e61f31866
icortez <icortez@ufg.edu.sv>
2021-04-27 11:34:00 -0600

    building tour component and sass

--- src/components/Tour/Tour.jsx
+++ src/components/Tour/Tour.jsx
@@ -2,8 +2,28 @@ import React from 'react';
 
 import './tour.scss';
 
-const Tour = () => {
-  return <h1>hello from Tour</h1>;
+const Tour = ({ city, img, name, info, removeTour }) => {
+  return (
+    <article className='tour'>
+      <div className='img-container'>
+        <img src={img} alt={name} />
+        <span className='close-btn'>
+          <i className='fas fa-window-close' />
+        </span>
+      </div>
+      <div className='tour-info'>
+        <h3>{city}</h3>
+        <h4>{name}</h4>
+        <h5>
+          info{' '}
+          <span>
+            <i className='fas fa-caret-square-down'></i>
+          </span>
+        </h5>
+        <p>{info}</p>
+      </div>
+    </article>
+  );
 };
 
 export default Tour;
